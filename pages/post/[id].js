dcb5bdeec7f23b3f8e83364eeb96493f37b64d5a
Kiverok <kiver566@ukr.net>
2022-10-05 19:32:36 +0300

    getInitialProps

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,9 +1,21 @@
 import { useRouter } from "next/router"
 import { MainLayout } from "../components/MainLayout"
+import Link from "next/dist/client/link"
 
-export default function Post() {
-    const router = useRouter()
+export default function Post({ post }) {
+    // const router = useRouter()
     return <MainLayout>
-    <h1>Post {router.query.id}</h1>
+    <h1>Post {post.title}</h1>
+    <hr />
+    <p>{post.body}</p>
+    <Link href={'/Posts'}><a>Back to all posts</a></Link>
     </MainLayout>
+}
+
+Post.getInitialProps = async (ctx) => {
+    const response =  await fetch(`http://localhost:4200/posts/${ctx.query.id}`)
+    const post = await response.json()
+    return {
+        post
+    }
 }
